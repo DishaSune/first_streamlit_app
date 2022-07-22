4dc27d54610a8515a59edb09dbb2e3e63603ec5d
DishaSune <109792795+DishaSune@users.noreply.github.com>
2022-07-22 18:25:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,6 @@ streamlit.text('2.kale,Spinach and Rocket Smoothie')
 streamlit.text('3.Hard boiled free-range egg')
 import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
 my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.dataframe(my_fruit_list)
+
